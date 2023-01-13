610aa6b42ec8f45ea19a393a2f0a585e646a550d
by-sabbir <me@sabbir.dev>
2023-01-13 12:37:53 +0600

    chore: expected result dto

---
+++ consolidatordto.go
@@ -0,0 +1,11 @@
+package main
+
+type ResultRow struct {
+	PackageName    string
+	Severity       string
+	Version        string
+	FixedInVersion string
+	Description    string
+	CVEID          string
+	Source         string
+}
